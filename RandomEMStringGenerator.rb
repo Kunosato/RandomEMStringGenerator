22421867b05c14fdb7ebd37d28b875f7fa804d82
Kunosato <cleciracsute@yahoo.co.jp>
2015-04-06 00:10:54 +0900

    全角英数を追加

--- RandomEMStringGenerator.rb
+++ RandomEMStringGenerator.rb
@@ -5,6 +5,6 @@ else
 end
 s = ''
 n.times {
-  s << ('ぁ'..'ん').to_a.concat(('ァ'..'ヴ').to_a).sample
+  s << ('ぁ'..'ん').to_a.concat(('ァ'..'ヴ').to_a).concat(('Ａ'..'Ｚ').to_a).concat(('ａ'..'ｚ').to_a).concat(('０'..'９').to_a).sample
 }
 puts s
